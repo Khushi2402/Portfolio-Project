29828d82bfef15fe938ed8438a9a9a83f97736d3
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2023-12-09 12:43:52 +0530

    Firebase Added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,57 @@ import React, {useState} from "react";
 
 const Contact = () => {
   const [userData, setUserData] = useState({
-    name: "",
+    fullname: "",
     email: "",
-    message: ","
+    message: ""
   });
 
-  const postUserData = () => {};
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUserData({ ...userData, [name]: value});
+  };
+
+  //connecting with firebase
+
+  const sendMessage = async(event) => {
+    event.preventDefault();
+    const {fullname, email, message} = userData;
+    
+    if (fullname && email && message) {
+    const res = 
+    fetch('https://krithika-portfolio-default-rtdb.firebaseio.com/userDataRecords.json',
+    {
+      method: "POST",
+      headers: {
+        "Context-Type" : "application/json",
+      },
+      body:JSON.stringify({
+        fullname,
+        email,
+        message,
+      }),
+    }
+    );
+
+    if(res) {
+      setUserData({
+        fullname: "",
+        email: "",
+        message: "",
+      })
+      alert("Message Sent");
+    }
+    else {
+      alert("Fill the Details");
+    }
+  }
+  else {
+    alert("Fill the Details");
+  }
+  };
 
     return(
         <div>
@@ -23,15 +68,16 @@ const Contact = () => {
       
     </div>
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
+    <form method="POST">
       <div class="flex flex-wrap -m-2">
-        <form method="POST">
+        
         <div class="font-nav p-2 w-1/2">
           <div class="relative">
             <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
             <input type="text" id="name" 
-            name="name" 
+            name="fullname" 
             class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            value={userData.name}
+            value={userData.fullname}
             onChange={postUserData}/>
           </div>
         </div>
@@ -56,9 +102,12 @@ const Contact = () => {
           </div>
         </div>
         <div class="p-2 w-full">
-          <button class="button flex mx-auto text-white bg-sky-700 border-0 py-2 px-8 focus:outline-none hover:bg-sky-800 rounded text-lg">Send</button>
+          <button 
+          class="button flex mx-auto text-white bg-sky-700 border-0 py-2 px-8 focus:outline-none hover:bg-sky-800 rounded text-lg"
+          onClick={sendMessage}>
+            Send
+            </button>
         </div>
-        </form>
         <div class="font-name  w-full pt-8 mt-8 border-t border-gray-200 text-center">
           <p class="text-slate-900 mt-2">+91 8767927688</p>
           <p class="text-slate-900 mt-2">kiyer8558@gmail.com</p>
@@ -67,6 +116,7 @@ const Contact = () => {
           
         </div>
       </div>
+      </form>
     </div>
   </div>
 </section>
